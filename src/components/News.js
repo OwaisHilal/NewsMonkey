9c895a532253c9e35c085c589954b77aed6e5d0f
OwaisHilal <owaishilal789@gmail.com>
2025-03-03 16:08:47 +0530

    Infinite Scroll

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Newsitem from './Newsitem';
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
   static defaultProps = {
@@ -22,6 +23,7 @@ export class News extends Component {
       articles: [],
       loading: false,
       page: 1,
+      totalResults: 0,
     };
     document.title = `${this.props.category} - NewsMonkey`;
   }
@@ -62,42 +64,67 @@ export class News extends Component {
     }
   };
 
+  fetchMoreData = async () => {
+    const nextPage = this.state.page + 1; // Store next page number
+
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a6c696ced67945c8afd474b6064cab55&page=${nextPage}&pageSize=${this.props.pageSize}`;
+
+    this.setState({ loading: true });
+
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles || []),
+      totalResults: parsedData.totalResults,
+      page: nextPage, // Update state with the correct next page number
+      loading: false,
+    });
+  };
+
   render() {
     return (
-      <div className="container my-3">
+      <>
         <h1 className="text-center" style={{ margin: '40px 0px' }}>
           NewsMonkey - Daily Updates on {this.props.category}
         </h1>
-        {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <Newsitem
-                    title={
-                      element.title ? element.title.slice(0, 45) : 'No Title'
-                    }
-                    description={
-                      element.description
-                        ? element.description.slice(0, 88)
-                        : 'No Description'
-                    }
-                    imageUrl={
-                      element.urlToImage ||
-                      'https://via.placeholder.com/150?text=No+Image'
-                    }
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
-        </div>
-
-        <div className="container d-flex justify-content-between">
+        {/* {this.state.loading && <Spinner />} */}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <Newsitem
+                      title={
+                        element.title ? element.title.slice(0, 45) : 'No Title'
+                      }
+                      description={
+                        element.description
+                          ? element.description.slice(0, 88)
+                          : 'No Description'
+                      }
+                      imageUrl={
+                        element.urlToImage ||
+                        'https://via.placeholder.com/150?text=No+Image'
+                      }
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+        {/* <div className="container d-flex justify-content-between">
           <button
             disabled={this.state.page <= 1}
             type="button"
@@ -117,8 +144,8 @@ export class News extends Component {
           >
             Next &rarr;
           </button>
-        </div>
-      </div>
+        </div> */}
+      </>
     );
   }
 }
